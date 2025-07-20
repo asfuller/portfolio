65b34f0cf03ed8039830a3bd805530e65464e143
asfuller <alexfuller123@yahoo.com>
2025-07-20 09:55:39 -0400

    Contacts CRUD capabilities
    This was done with React, Flask, and Python

--- README.md
+++
@@ -1,9 +0,0 @@
-# Portfolio
-Hello, my name is Alex Fuller and this is my portfolio site.
-
-Here, you can view my past and current projects, resume, and more about me.
-
-If you would like to work with me on a project, you can fill out the contact form at the bottom of my site and I will reach you as soon as I can.
-
-# Closing
-Thank you for visiting! Have a wonderful day! 😃
---
+++ backend/models.py
@@ -0,0 +1,17 @@
+# The database models / how we interact with the DB
+from config import db
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    #Take our object data fields from above into a Python dictionary then convert to json
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email
+        }
